8ed5e352d23165cf50b7ed5438cd0dc50783b9ec
Ekansh Rastogi <ekansh@ekiras.com>
2017-07-24 23:14:58 +0530

    udpate markdown imports

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,14 @@ export { MarkdownItComponent } from 'components/MarkdownItComponent';
 export { MarkdownItConfig } from './config/MarkdownItConfig';
 export { MarkdownItService } from './services/MarkDownItService';
 
-import * as md from 'markdown-it';
-import * as mdContainer from 'markdown-it-container';
-import * as hljs from 'markdown-it-highlightjs';
-import * as mdBlockImage from 'markdown-it-block-image';
-import * as mdBlockEmbed from 'markdown-it-block-embed';
+import md from 'markdown-it';
+import mdContainer from 'markdown-it-container';
+import hljs from 'markdown-it-highlightjs';
+import mdBlockImage from 'markdown-it-block-image';
+import mdBlockEmbed from 'markdown-it-block-embed';
 
-export { md as markdown };
-export { mdContainer as markdownContainer };
-export { hljs as languageHighlighter };
-export { mdBlockImage as markdownBlockImage };
-export { mdBlockEmbed as markdownBlockEmbed };
-
+export const markdown = new md();
+export const markdownContainer = new mdContainer();
+export const languageHighlighter = new hljs();
+export const markdownBlockImage = new mdBlockImage();
+export const markdownBlockEmbed = new mdBlockEmbed();
